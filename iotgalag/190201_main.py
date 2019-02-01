2aa2214d392bc3dd49db08586b92ac9a2d54059d
lpmarz3 <lpmarz3@naver.com>
2019-02-01 15:21:06 +0900

    backdraw funtion makin

--- iotgalag/190201_main.py
+++ iotgalag/190201_main.py
@@ -4,17 +4,22 @@ WHITE         = (48, 48, 48)
 displaywidth  = 470
 displayheight = 840
 displayobj    = None
+imgback       = pygame.image.load('image/back.png')
 
 
 def iotsetcaption(caption):
     pygame.display.set_caption(caption)
 
-def base():
+def iotbackdraw():
     global displayobj
+    global imgback
 
-    pygame.init()
-    iotsetcaption("IoT Game")
-    displayobj = pygame.display.set_mode((displaywidth, displayheight))
+    displayobj.blit(imgback, (0, 0))
+
+def iotgo():
+    global displayobj
+    #global clock
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -23,5 +28,19 @@ def base():
         if key[pygame.K_SPACE]:
             pygame.quit()
             break
+        displayobj.fill(WHITE)
+        iotbackdraw()
+        pygame.display.update()
+    pygame.quit()
+
+def base():
+    global displayobj
+    #global clock
+
+    pygame.init()
+    iotsetcaption("IoT Game")
+    displayobj = pygame.display.set_mode((displaywidth, displayheight))
+
+    iotgo()
 
 base()
